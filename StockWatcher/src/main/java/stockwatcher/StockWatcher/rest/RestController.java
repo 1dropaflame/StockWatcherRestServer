e1386bec50a2815a939ff4530fdf528c083a7619
anil <1dropaflame@gmail.com>
2021-05-14 12:25:49 -0500

    StockWatcher REST server. SpringBoot.

---
+++ StockWatcher/src/main/java/stockwatcher/StockWatcher/rest/RestController.java
@@ -0,0 +1,29 @@
+package stockwatcher.StockWatcher.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import stockwatcher.StockWatcher.domain.StockPrice;
+
+@org.springframework.web.bind.annotation.RestController
+@RequestMapping("")
+public class RestController {
+    public StockPrice[] prices = new StockPrice[] {new StockPrice("AMZN", 100, 0.3), new StockPrice("MSFT", 10, 0.5)};
+
+    @GetMapping("/stockPrices")
+    public StockPrice[] getPrices(@RequestParam(value="symbols") String[] symbols) {
+        if(symbols.length > 0) {
+            prices = new StockPrice[symbols.length];
+            for(int i=0; i<symbols.length; i++) {
+                prices[i] = new StockPrice(symbols[i], 100, 0.3);
+            }
+            return prices;
+        }
+        return prices; // default
+    }
+    @GetMapping("/{id}")
+    public Integer hello(@PathVariable int id) {
+        return ++id;
+    }
+}

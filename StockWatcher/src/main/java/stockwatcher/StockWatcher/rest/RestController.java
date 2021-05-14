a267bcf13ef49bbfce173a90150df5285f8e3c55
anil <1dropaflame@gmail.com>
2021-05-14 13:05:00 -0500

    @CrossOrigin added.

--- StockWatcher/src/main/java/stockwatcher/StockWatcher/rest/RestController.java
+++ StockWatcher/src/main/java/stockwatcher/StockWatcher/rest/RestController.java
@@ -1,11 +1,9 @@
 package stockwatcher.StockWatcher.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import stockwatcher.StockWatcher.domain.StockPrice;
 
+@CrossOrigin
 @org.springframework.web.bind.annotation.RestController
 @RequestMapping("")
 public class RestController {
